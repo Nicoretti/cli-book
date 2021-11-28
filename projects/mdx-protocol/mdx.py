df6ebf595c8783273d422c7ffa17c200495ff4da
Nicola Coretti <nico.coretti@gmail.com>
2021-11-28 14:51:45 +0100

    Add Channel and Header encoder and decoder

--- projects/mdx-protocol/mdx.py
+++ projects/mdx-protocol/mdx.py
@@ -1,39 +1,169 @@
 #!/bin/python
-import enum
+import os
 import sys
+import argparse
 from enum import IntEnum
+from struct import unpack, pack, calcsize
 from itertools import chain
 
 
-class Channel:
-    VERSION = 1
+class DecodeError(Exception):
+    pass
 
-    def __init__(self, mux_id, data):
+
+class Header:
+    VERSION = 1
+    RESERVED = 0
+    BINARY_FORMAT = '>HHHHI'
+
+    def __init__(self, mux_id, length, reserved=RESERVED, version=VERSION):
+        self._version = version
+        self._reserved = reserved
         self._mux_id = mux_id
-        self._data = data
+        self._length = length
+
+    @property
+    def version(self):
+        return self._version
+
+    @property
+    def reserved(self):
+        return self._reserved
+
+    @property
+    def mux_id(self):
+        return self._mux_id
+
+    @property
+    def checksum(self):
+        return 0
+
+    @property
+    def length(self):
+        return self._length
 
     def __bytes__(self):
-        reserved = 0
-        checksum = 0
-        length = len(self._data)
+        return pack(
+            Header.BINARY_FORMAT,
+            self.version,
+            self.checksum,
+            self.reserved,
+            self.mux_id,
+            self.length
+        )
+
+    @staticmethod
+    def from_bytes(src):
+        if len(src) != calcsize(Header.BINARY_FORMAT):
+            raise DecodeError("Invalid amount of input bytes")
+        version, checksum, reserved, mux_id, length = unpack(
+            Header.BINARY_FORMAT,
+            src
+        )
+        return Header(mux_id=mux_id, length=length, reserved=reserved, version=version)
+
+
+class Channel:
+
+    def __init__(self, mux_id, data):
+        self._header = Header(mux_id=mux_id, length=len(data))
+        self._data = bytes(data)
+
+    @property
+    def mux_id(self):
+        return self._header.mux_id
+
+    @property
+    def data(self):
+        return self._data
+
+    def __eq__(self, other):
+        return self.mux_id == other.mux_id and self.data == other.data
+
+    def __ne__(self, other):
+        return not (self == other)
+
+    def __bytes__(self):
         return bytes(chain.from_iterable((
-            self.VERSION.to_bytes(2, 'big'),
-            reserved.to_bytes(2, 'big'),
-            checksum.to_bytes(2, 'big'),
-            self._mux_id.to_bytes(2, 'big'),
-            length.to_bytes(4, 'big'),
-            self._data
+            bytes(self._header),
+            self.data
         )))
+
+    @staticmethod
+    def from_bytes(data):
+        size = calcsize(Header.BINARY_FORMAT)
+        header = Header.from_bytes(data[0:size])
+        remaining = data[size:]
+        if len(remaining) < header.length:
+            raise DecodeError("Not enough bytes to decode message")
+        return Channel(mux_id=header.mux_id, data=remaining[0:header.length])
 
 
 class Cli:
-    class ExitCode(enum.IntEnum):
+    class ExitCode(IntEnum):
         SUCCESS = 0
         FAILURE = -1
 
+    class File:
+
+        def __init__(self, mode='r', bufsize=-1, encoding=None, errors=None):
+            self._mode = mode
+            self._bufsize = bufsize
+            self._encoding = encoding
+            self._errors = errors
+
+        def __call__(self, string):
+            dispatch = {
+                'r': lambda: sys.stdin,
+                'rb': lambda: os.fdopen(sys.stdin.fileno(), 'rb', closefd=False),
+                'w': lambda: sys.stdout,
+                'wb': lambda: os.fdopen(sys.stdout.fileno(), 'wb', closefd=False)
+            }
+            if string == '-':
+                try:
+                    return dispatch[self._mode]()
+                except KeyError as ex:
+                    raise ValueError(f'argument "-" with mode {self._mode}')
+            try:
+                return open(string, self._mode, self._bufsize, self._encoding,
+                            self._errors)
+            except OSError as e:
+                args = {'filename': string, 'error': e}
+                message = "can't open '{filename}s': {error}s"
+                raise argparse.ArgumentTypeError(message.format(args))
+
     def main(self, argv=None):
-        sys.exit(Cli.ExitCode.SUCCESS)
+        p = self.create_parser()
+        args = p.parse_args(argv)
+        args.func(args)
+        return Cli.ExitCode.SUCCESS
+
+    @staticmethod
+    def decode(settings):
+        return NotImplementedError()
+
+    @staticmethod
+    def encode(settings):
+        return NotImplementedError()
+
+    @staticmethod
+    def create_parser():
+        parser = argparse.ArgumentParser("mdx", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        sub = parser.add_subparsers(dest='subcommand', required=True, metavar='subcommand', help='[encode, decode]')
+
+        decode = sub.add_parser('decode', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        decode.add_argument('input', type=Cli.File('rb'), default='-', help='binary input')
+        decode.add_argument('output', type=Cli.File('w'), default='-', help='decoded output')
+        decode.set_defaults(func=Cli.decode)
+
+        encode = sub.add_parser('encode', formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+        encode.add_argument('output', type=Cli.File('wb'), default='-', help='binary output')
+        encode.add_argument('input', type=Cli.File('rb'), default='-', help='input(s) to be encoded')
+        encode.add_argument('-c', '--chunk-size', type=int, default=1024, help='in bytes')
+        encode.set_defaults(func=Cli.encode)
+
+        return parser
 
 
 if __name__ == '__main__':
-    Cli.main()
+    sys.exit(Cli().main())
--- projects/mdx-protocol/mdx_test.py
+++ projects/mdx-protocol/mdx_test.py
@@ -11,10 +11,31 @@ def test_encode_channel_to_bytes():
         0x00, 0x00,  # checksum
         0x00, 0x00,  # reserved
         0x00, 0x01,  # mux_id
-        0x00, 0x00, 0x00, 0x0A,  # mux_id
+        0x00, 0x00, 0x00, 0x0A,  # length
         # data
         0x00, 0x01, 0x02, 0x03,
         0x04, 0x05, 0x06, 0x07,
         0x08, 0x09
     ])
-    assert expected == bytes(channel)
+    actual = bytes(channel)
+    assert expected == actual
+
+
+def test_decode_channel_from_bytes():
+    data = bytes([
+        0x00, 0x01,  # version
+        0x00, 0x00,  # checksum
+        0x00, 0x00,  # reserved
+        0x00, 0x01,  # mux_id
+        0x00, 0x00, 0x00, 0x0A,  # length
+        # data
+        0x00, 0x01, 0x02, 0x03,
+        0x04, 0x05, 0x06, 0x07,
+        0x08, 0x09
+    ])
+    expected = Channel(
+        mux_id=1,
+        data=bytes([0, 1, 2, 3, 4, 5, 6, 7, 8, 9])
+    )
+    actual = Channel.from_bytes(data)
+    assert expected == actual
